cd93e93f519c4ea45082dc47a545e4a87adbfd6b
ww <wangwei@xiangha.com>
2020-01-21 16:17:40 +0800

    init

---
+++ main.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"github.com/BurntSushi/toml"
+	"log"
+	"os"
+	"sync"
+)
+
+var once sync.Once
+var confPath = "./conf/"
+var Data map[string]interface{}
+
+func init() {
+	once.Do(func() {
+		if !Exists(confPath) {
+			if Exists("../../conf/") {
+				confPath = "../../conf/"
+			}
+		}
+		if _, err := toml.DecodeFile(confPath+"config.toml", &Data); err != nil {
+			log.Fatal(err)
+		}
+	})
+}
+
+func Exists(path string) bool {
+	_, err := os.Stat(path) //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
+}
